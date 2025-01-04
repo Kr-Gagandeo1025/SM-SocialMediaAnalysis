a18fad11d9af01ee2179c42dec3d31f9481b76d2
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2025-01-04 17:12:39 +0530

    merge commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className="flex justify-between items-center">
       <div className="flex items-center">
         <img src="Brainwave.png" alt="icon" className=""/>
-        <span className="ml-2 text-gray-900 font-bold text-2xl">SocialScope</span>
+        <span className="ml-2 text-gray-900 font-bold text-3xl">SOCIAL SCOPE.</span>
       </div>
       <div className="flex items-center">
         <img src="user.png" alt="user" className="h-8 rounded-full"/>
